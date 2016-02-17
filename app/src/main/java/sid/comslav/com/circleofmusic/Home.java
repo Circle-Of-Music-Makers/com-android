6d669e78939830a386682d40f8387db07cfa2ff9
Saumil <saumil.sachdeva@gmail.com>
2016-02-18 02:01:08 +0530

    UploadFile code added. (Not Tested)

--- app/src/main/java/sid/comslav/com/circleofmusic/Home.java
+++ app/src/main/java/sid/comslav/com/circleofmusic/Home.java
@@ -82,74 +82,6 @@ public class Home extends AppCompatActivity {
             intent_upload.setAction(Intent.ACTION_GET_CONTENT);
             startActivityForResult(intent_upload,1);
 
-            HttpURLConnection connection;
-            connection = null;
-            DataOutputStream outputStream;
-            outputStream = null;
-            DataInputStream inputStream = null;
-            String pathToOurFile = "uri";
-            String urlServer = "http://circleofmusic-sidzi.rhcloud.com/uploadFiles";
-            String lineEnd = "\r\n";
-            String twoHyphens = "--";
-            String boundary =  "*****";
-
-            int bytesRead, bytesAvailable, bufferSize;
-            byte[] buffer;
-            int maxBufferSize = 1024*1024;
-
-            try
-            {
-                FileInputStream fileInputStream = new FileInputStream(new File(pathToOurFile) );
-
-                URL url = new URL(urlServer);
-                connection = (HttpURLConnection) url.openConnection();
-
-                // Allow Inputs &amp; Outputs.
-                connection.setDoInput(true);
-                connection.setDoOutput(true);
-                connection.setUseCaches(false);
-
-                // Set HTTP method to POST.
-                connection.setRequestMethod("POST");
-
-                connection.setRequestProperty("Connection", "Keep-Alive");
-                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
-
-                outputStream = new DataOutputStream( connection.getOutputStream() );
-                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-                outputStream.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + pathToOurFile +"\"" + lineEnd);
-                outputStream.writeBytes(lineEnd);
-
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                buffer = new byte[bufferSize];
-
-                // Read file
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                while (bytesRead > 0)
-                {
-                    outputStream.write(buffer, 0, bufferSize);
-                    bytesAvailable = fileInputStream.available();
-                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-                }
-
-                outputStream.writeBytes(lineEnd);
-                outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-                // Responses from the server (code and message)
-                int serverResponseCode = connection.getResponseCode();
-                String serverResponseMessage = connection.getResponseMessage();
-
-                fileInputStream.close();
-                outputStream.flush();
-                outputStream.close();
-            }
-            catch (Exception ex)
-            {
-                //Exception handling
-            }
 
             return true;
         }
@@ -192,6 +124,75 @@ public class Home extends AppCompatActivity {
 
                 //the selected audio.
                 uri = data.getData();
+
+                HttpURLConnection connection;
+                connection = null;
+                DataOutputStream outputStream;
+                outputStream = null;
+                DataInputStream inputStream = null;
+                String pathToOurFile = uri.toString();
+                String urlServer = "http://circleofmusic-sidzi.rhcloud.com/uploadFiles";
+                String lineEnd = "\r\n";
+                String twoHyphens = "--";
+                String boundary =  "*****";
+
+                int bytesRead, bytesAvailable, bufferSize;
+                byte[] buffer;
+                int maxBufferSize = 1024*1024;
+
+                try
+                {
+                    FileInputStream fileInputStream = new FileInputStream(new File(pathToOurFile) );
+
+                    URL url = new URL(urlServer);
+                    connection = (HttpURLConnection) url.openConnection();
+
+                    // Allow Inputs &amp; Outputs.
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    connection.setUseCaches(false);
+
+                    // Set HTTP method to POST.
+                    connection.setRequestMethod("POST");
+
+                    connection.setRequestProperty("Connection", "Keep-Alive");
+                    connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+
+                    outputStream = new DataOutputStream( connection.getOutputStream() );
+                    outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                    outputStream.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + pathToOurFile +"\"" + lineEnd);
+                    outputStream.writeBytes(lineEnd);
+
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    buffer = new byte[bufferSize];
+
+                    // Read file
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    while (bytesRead > 0)
+                    {
+                        outputStream.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                    }
+
+                    outputStream.writeBytes(lineEnd);
+                    outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                    // Responses from the server (code and message)
+                    int serverResponseCode = connection.getResponseCode();
+                    String serverResponseMessage = connection.getResponseMessage();
+
+                    fileInputStream.close();
+                    outputStream.flush();
+                    outputStream.close();
+                }
+                catch (Exception ex)
+                {
+                    //Exception handling
+                }
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
