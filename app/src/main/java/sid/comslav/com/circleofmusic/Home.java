d43469963cddafa427174ad6f31bdf83f362cde3
Siddhant <rahulgupta388@gmail.com>
2016-04-23 21:12:14 +0530

    Working on Upload Halfway through

--- app/src/main/java/sid/comslav/com/circleofmusic/Home.java
+++ app/src/main/java/sid/comslav/com/circleofmusic/Home.java
@@ -1,10 +1,12 @@
 package sid.comslav.com.circleofmusic;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -12,8 +14,10 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,22 +27,29 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import net.gotev.uploadservice.BinaryUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+import net.gotev.uploadservice.UploadService;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
+
 
 public class Home extends AppCompatActivity {
     int count;
     String songs[], songpath;
     JSONObject obj;
     Uri uri;
+    private static final int READ_REQUEST_CODE = 42;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        UploadService.NAMESPACE = BuildConfig.APPLICATION_ID;
         if (!isNetworkAvailable()) {
             // Internet Connection is not present
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -135,14 +146,19 @@ public class Home extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.upload) {
 
-            Intent intent_upload = new Intent();
-            intent_upload.setType("audio/*");
-            intent_upload.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(intent_upload, 1);
-            //doFileUpload();
+//            Intent intent_upload = new Intent();
+//            intent_upload.setType("audio/*");
+//            intent_upload.setAction(Intent.ACTION_GET_CONTENT);
+//            startActivityForResult(intent_upload, 1);
+//            //doFileUpload();
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("audio/*");
+            startActivityForResult(intent, READ_REQUEST_CODE);
 
             return true;
         }
@@ -151,20 +167,43 @@ public class Home extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
 
-        if (requestCode == 1) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Cursor cursor = null;
+                try {
+                    cursor = getApplicationContext().getContentResolver().query(uri, new String[]{MediaStore.Audio.Media.DATA}, null, null, null);
+                    if (cursor != null) {
+                        cursor.moveToFirst();
+                        Log.i("Cursor Out", cursor.getString(1));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (cursor != null)
+                        cursor.close();
+                }
+                try {
+                    String uploadId =
+                            new BinaryUploadRequest(getApplicationContext(), "http://circleofmusic-sidzi.rhcloud.com/upYourTrack")
+                                    .setFileToUpload("")
+                                    .setNotificationConfig(new UploadNotificationConfig())
+                                    .setMaxRetries(2)
+                                    .startUpload();
+                } catch (Exception exc) {
+                    Log.e("AndroidUploadService", exc.getMessage(), exc);
+                }
 
-            if (resultCode == RESULT_OK) {
-
-                //the selected audio.
-                uri = data.getData();
-                songpath = uri.toString();
-                UploadSong song = new UploadSong();
-                song.execute(songpath);
             }
         }
-        super.onActivityResult(requestCode, resultCode, data);
+
     }
 
     private class TrackAdapter extends BaseAdapter {
---
+++ app/src/main/java/sid/comslav/com/circleofmusic/SongUploader.java
@@ -0,0 +1,15 @@
+package sid.comslav.com.circleofmusic;
+
+import android.os.AsyncTask;
+
+public class SongUploader extends AsyncTask<String, Void, String> {
+
+
+    @Override
+    protected String doInBackground(String... params) {
+
+
+        return null;
+    }
+
+}
