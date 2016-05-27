3128a37273f2173789e6e49e12247821815d0087
sidzi <rahulgupta388@gmail.com>
2016-05-28 00:56:00 +0530

    Update ListFileActivity.java

--- app/src/main/java/sid/comslav/com/circleofmusic/ListFileActivity.java
+++ app/src/main/java/sid/comslav/com/circleofmusic/ListFileActivity.java
@@ -23,9 +23,9 @@ public class ListFileActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_files);
-        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
                 String[] perms = {"android.permission.READ_EXTERNAL_STORAGE"};
                 requestPermissions(perms, 200);
             }
